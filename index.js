f89f9d89ffdc7a4076adbf84b8f1ca6628656165
Jordy Slagter <slagterj2001@gmail.com>
2025-05-16 11:03:56 +0200

    initial commit, add CI

---
+++ index.js
@@ -0,0 +1,7 @@
+﻿function get_hello() {
+  return "Hello World!";
+}
+
+module.exports = get_hello;
+
+console.log(get_hello());
---
+++ index.test.js
@@ -0,0 +1,7 @@
+﻿const get_hello = require("./index");
+
+describe("get_hello()", () => {
+  it("returns 'Hello, World!' string", () => {
+    expect(get_hello()).toEqual("Hello World!");
+  });
+});
