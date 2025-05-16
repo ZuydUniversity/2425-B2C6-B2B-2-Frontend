5ab93aef2e9a7962c1e72d640800bf0727d76dac
Jordy Slagter <slagterj2001@gmail.com>
2025-05-16 13:46:24 +0200

    add homepage test

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,10 @@
+﻿import HomePage from "./index";
+import {render} from "@testing-library/react";
+
+describe("HomePage", () => {
+  it("renders an h1 element", () => {
+    const { container } = render(<HomePage />);
+    const h1 = container.querySelector("h1");
+    expect(h1).not.toBeNull();
+  });
+});
