ec8c5f3d1ba7859d6ea5b6ffdcc8fa2aadd92651
sven <svencl20@gmail.com>
2025-05-31 15:17:23 +0200

    formatting fix test 2

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 const TestComponent: React.FC = () => {
-  return (
-    <div data-testid="test-component">CI/CD meting – versie 2
-</div>
-  );
+  return <div data-testid="test-component">CI/CD meting – versie 2</div>;
 };
 
 export default TestComponent;
