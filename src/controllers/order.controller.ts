1dbad2c44dab82c506398473fe5055242c7606d1
Jordy Slagter <slagterj2001@gmail.com>
2025-06-26 10:27:08 +0200

    add getOneById

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -39,9 +39,46 @@ export default class OrderController {
         return EitherModule.right(result);
       })
       .catch((error) => {
-        return EitherModule.left(error);
+        return EitherModule.left(error.toString());
       });
 
     return EitherModule.right([]);
+  }
+
+  public static getOneById(
+    idToGet: number,
+  ): EitherModule.Either<string, Order[]> {
+    axios
+      .get<any>("https://10.0.2.4:8080/api/Orders/" + idToGet)
+      .then((response) => {
+        const item = response.data;
+
+        const order = new Order({
+          id: item["id"] as number,
+          customerId: item["customerId"] as number,
+          productId: item["productId"] as number,
+          quantity: item["quantity"] as number,
+          totalPrice: item["totalPrice"] as number,
+          status: item["status"] as string,
+          orderDate: item["orderDate"] as Date,
+          approvedDate: item["approvedDate"] as Date,
+          rejectedDate: item["rejectedDate"] as Date,
+          deliveredDate: item["deliveredDate"] as Date,
+          comment: item["comment"] as string,
+          orderType: item["orderType"] as string,
+          isSignedByInkoop: item["isSignedByInkoop"] as boolean,
+          isSignedByAccountmanager: item["isSignedByAccountmanager"] as boolean,
+          forwardedToSupplier: item["forwardedToSupplier"] as boolean,
+          picklistStatus: item["picklistStatus"] as string,
+          rejectionReason: item["rejectionReason"] as string,
+        });
+
+        return EitherModule.right(order);
+      })
+      .catch((error) => {
+        return EitherModule.left(error.toString());
+      });
+
+    return EitherModule.left("No Order found with that id");
   }
 }
